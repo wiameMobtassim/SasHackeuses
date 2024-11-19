ad2545fc94e1c9c3dc27f271e0114c17dc60240a
wiameMobtassim <wiamemobtassim@gmail.com>
2024-11-19 15:38:26 +0000

    ExerciceArrays_Objects

--- ExerciceArrays_Objects.js
+++ ExerciceArrays_Objects.js
@@ -1,6 +1,11 @@
 //Exercice 1
-let nombres = [1,2,3,4,5];
+let nombres = [1, 2, 3, 4, 5];
 console.log(nombres);
 //Exercice 2
 let fruits = ["apple","banana","cherry"];
 console.log(fruits[1]);
+//Exercice 3 
+let nbrs = [10, 20, 30, 40];
+console.log(nbrs);
+nbrs[2] = 50;
+console.log(nbrs);
