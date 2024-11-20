8293003f5d9e9d2fb94b990045c70516135e6b2b
wiameMobtassim <wiamemobtassim@gmail.com>
2024-11-20 11:50:56 +0000

    Exercice 12

--- ExerciceArrays_ObjectsMoyens.js
+++ ExerciceArrays_ObjectsMoyens.js
@@ -4,3 +4,12 @@ let elements = [1,2,3,4,5];
 for (let ele in elements){
     console.log(ele);
 }
+console.log("-------------12");
+//Exercice 12
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+function estPair(n) {
+    return n %2 === 0 ;
+}
+let numbersPairs = numbers.filter(estPair);
+console.log("les nombres " + numbers);
+console.log("les nombres pairs est "+ numbersPairs);
