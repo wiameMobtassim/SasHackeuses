80a86213522136f4b352cbdad4ef57d4142d9221
wiameMobtassim <wiamemobtassim@gmail.com>
2024-11-20 10:38:19 +0000

    Exercice 2

--- ExerciceArrays_ObjectsFaciles.js
+++ ExerciceArrays_ObjectsFaciles.js
@@ -2,3 +2,7 @@ console.log("----------------1");
 //Exercice 1
 let nombres = [1, 2, 3, 4, 5];
 console.log(nombres);
+console.log("----------------2");
+//Exercice 2
+let fruits = ["apple","banana","cherry"];
+console.log(fruits[1]);
